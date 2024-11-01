130c4e96e54a66ad69469a6fe807c2de9ea4d86a
owenharvey7 <oharvey347@gmail.com>
2024-11-01 11:57:17 -0400

    Daily Question Complete

---
+++ LeetCode/DeleteCharachtersToMakeFancyString.py
@@ -0,0 +1,47 @@
+"""
+A fancy string is a string where no three consecutive characters are equal.
+Given a string s, delete the minimum possible number of characters from s to make it fancy.
+Return the final string after the deletion. It can be shown that the answer will always be unique.
+
+Example 1:
+Input: s = "leeetcode"
+Output: "leetcode"
+Explanation:
+Remove an 'e' from the first group of 'e's to create "leetcode".
+No three consecutive characters are equal, so return "leetcode".
+
+Example 2:
+Input: s = "aaabaaaa"
+Output: "aabaa"
+Explanation:
+Remove an 'a' from the first group of 'a's to create "aabaaaa".
+Remove two 'a's from the second group of 'a's to create "aabaa".
+No three consecutive characters are equal, so return "aabaa".
+
+Example 3:
+Input: s = "aab"
+Output: "aab"
+Explanation: No three consecutive characters are equal, so return "aab".
+
+Constraints:
+1 <= s.length <= 105
+s consists only of lowercase English letters.
+Difficulty: Easy
+"""
+
+class Solution(object):
+    def makeFancyString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = [s[0]]
+        count = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                count += 1
+            else:
+                count = 1
+            if count < 3:
+                res.append(s[i])
+        return ''.join(res)
